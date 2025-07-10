f826f3c1d078a05acdbfd8fb8c920a1bb0027b91
WillianJefferson <willian100@gmail.com>
2025-07-09 21:36:33 -0300

    Commit inicial

---
+++ menu.php
@@ -0,0 +1,22 @@
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="index.php">Plataforma Cursos</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
+                            <li class="nav-item"><a class="nav-link" href="sobre.php">Sobre</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Cursos</a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
+                                    <li><a class="dropdown-item" href="frontend.php">Frontend</a></li>
+                                    <li><a class="dropdown-item" href="backend.php">Backend</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="contato.php">Contato</a></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
